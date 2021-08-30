9926526804f510a945bf885f228244f4318c6a41
nastya <nastyai137@gmail.com>
2021-08-30 12:37:36 +0300

    movies class

--- sourceCode.js
+++ sourceCode.js
@@ -1,69 +1,22 @@
 "use strict";
 
-class Book {
-    constructor(title, author, isbn, numCopies) {
-        this._title = title;
-        this._author = author;
-        this._isbn = isbn;
-        this._numCopies = numCopies;
-    };
-
-    getAvailability() {
-        let _isAvailable = (this._numCopies === 0) ? "out of stock" : (this._numCopies < 10) ? "lowstock" : "in stock";
-
-        return this._title + " " + _isAvailable;
-    };
-
-    get availability() {
-        return this.getAvailability();
+class Movie {
+    constructor(Title, Director, Genre, ReleaseYear, Rating) {
+        this.movie = Title;
+        this.director = Director;
+        this.genre = Genre;
+        this.releaseYear = ReleaseYear;
+        this.rating = Rating
     }
 
-    getTitle() {
-        return this._title;
-    }
-
-    sell(numSold = 1) {
-        if (this._numCopies === 0) {
-            this.getAvailability();
-        } else if (this._numCopies < numSold) {
-            return "We don't have so much copies to sell"
-        } else {
-            this._numCopies -= numSold;
-            return "succesfully sold";
-        }
-    };
-
-    restock(numCopies = 5) {
-        this._numCopies += numCopies;
+    getOverview() {
+        return `${this.movie}, a ${this.genre} film directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${this.rating}`;;
     }
 };
 
-let warAndPiece = new Book("War and Piece", "Tolstoy", 546393, 6);
-let soyer = new Book("Tom Soyer", "Mark Twen", 6854, 78);
-let kastaneda = new Book("Don Huan", "Kastaneda Karlos", 2744, 10);
-let harry = new Book("Harry Potter", "Joan Rowling", 4567, 60);
+const jackMovie = new Movie('The House That Jack built', 'Lars von Trier', 'triller', '2018', '7.0');
 
-console.log(warAndPiece.availability); //low stock
-console.log(warAndPiece.sell(10)); //we don't have
-warAndPiece.restock(10);
-console.log(warAndPiece.getAvailability()); //in stock
+const puaro = new Movie('Poirot', 'Edvard Bennet', 'detective', 1989, 8.2);
 
-console.log(soyer.getAvailability()); //in stock
-console.log(soyer.sell(77));
-console.log(soyer.sell());
-console.log(soyer.getAvailability());//out of stock
-
-console.log(kastaneda.getAvailability()); //in stock
-console.log(kastaneda.sell(5));
-console.log(kastaneda.getAvailability()); //low stock
-
-console.log(harry.getAvailability()); //in stock
-console.log(harry.sell(60)); 
-console.log(harry.getAvailability()); //out of stock
-harry.restock(5);
-console.log(harry.getAvailability()); //low stock
-
-
-
-
-
+console.log(jackMovie.getOverview());
+console.log(puaro.getOverview());
